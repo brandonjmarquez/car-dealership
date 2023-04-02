dce33181f68e71d39bbddf0f021b3ae0692497f6
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-04-01 19:08:30 -0500

    Started working on dropdown for inventory filters.

--- src/pages/Inventory/Inventory.tsx
+++ src/pages/Inventory/Inventory.tsx
@@ -2,17 +2,17 @@ import React, { useMemo, useRef } from 'react';
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import { CARS } from "../../tools/mock-cars";
+import SortingModal from '../../components/SortingModal/SortingModal';
 
 // import logo from './logo.svg';
 
 
 function Inventory() {
-
   const inventory = useMemo(() => {
     let items: JSX.Element[] = [];
     CARS.forEach((item) => {
       items.push(
-        <div>
+        <div key={`${item.year}${item.make}${item.model}${item.id}`}>
           <img src={item.img} width="100%"
             className="object-cover"
             srcSet={`${item.img}-400 400w,
@@ -26,20 +26,41 @@ function Inventory() {
     })
     return items;
   }, [])
+  
+  const sorting = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // const closeCarInfo = () => {
+    //   setInfoModal(createPortal(
+    //     <SortingModal /> , 
+        
+    //   ))
+    // }
+    console.log((e.target as HTMLButtonElement).parentElement!.id);
+    // closeCarInfo();
+  }
 
   return (
     <>
       <Navbar />
       <div className="flex flex-col text-white">
-        <h1 className="text-xl md:text-6xl font-bold m-5 p-5 border-b-2">Inventory</h1>
-        <div className="grid grid-cols-[minmax(30%,_.5fr)_minmax(70%,_1fr)] m-5">
-          <div>
-            <div className="grid grid-rows-4 mr-4">
-              <button className="my-1 px-5 py-3 bg-custom-300 rounded text-left">Year</button>
-              <button className="my-1 px-5 py-3 bg-custom-300 rounded text-left">Make</button>
-              <button className="my-1 px-5 py-3 bg-custom-300 rounded text-left">Model</button>
-              <button className="my-1 px-5 py-3 bg-custom-300 rounded text-left">Body</button>
-              <button className="my-1 px-5 py-3 bg-custom-300 rounded text-left">Color</button>
+        <h1 className="text-3xl md:text-6xl font-bold m-5 p-5 border-b-2 whitespace-nowrap">Inventory</h1>
+        <div className="grid grid-rows-2 sm:grid-cols-[minmax(30%,_.5fr)_minmax(70%,_1fr)] m-5">
+          <div className="">
+            <div className="grid grid-cols-5 sm:grid-rows-5 sm:grid-cols-none mr-4">
+              <div id="year">
+                <button className="w-full active:bg-custom-400 m-1 px-5 py-3 bg-custom-300 rounded text-left" onClick={(e) => sorting(e)}>Year</button>
+              </div>
+              <div id="make">
+                <button className="w-full active:bg-custom-400 m-1 px-5 py-3 bg-custom-300 rounded text-left" onClick={(e) => sorting(e)}>Make</button>
+              </div>
+              <div id="model">
+                <button className="w-full active:bg-custom-400 m-1 px-5 py-3 bg-custom-300 rounded text-left" onClick={(e) => sorting(e)}>Model</button>
+              </div>
+              <div id="body">
+                <button className="w-full active:bg-custom-400 m-1 px-5 py-3 bg-custom-300 rounded text-left" onClick={(e) => sorting(e)}>Body</button>
+              </div>
+              <div id="color">
+                <button className="w-full active:bg-custom-400 m-1 px-5 py-3 bg-custom-300 rounded text-left" onClick={(e) => sorting(e)}>Color</button>
+              </div>
             </div>
           </div>
           <div>
